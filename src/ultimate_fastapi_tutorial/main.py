23b803c527b6655d63c0b769ac95b50e6771851d
Derek Wan <d.wan@icloud.com>
2022-10-04 22:18:31 +0800

    Part 2

--- src/ultimate_fastapi_tutorial/main.py
+++ src/ultimate_fastapi_tutorial/main.py
@@ -6,6 +6,29 @@ from fastapi import FastAPI
 from fastapi import status
 
 
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/"
+        + "cauliflower-and-tofu-curry-recipe.html",
+    },
+]
+
+
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 
@@ -20,6 +43,17 @@ def root() -> dict[str, Any]:
     """
 
     return {"msg": "Hello, World!"}
+
+
+@api_router.get("/recipe/{recipe_id}", status_code=status.HTTP_200_OK)
+def fetch_recipe(*, recipe_id: int) -> dict[str, Any] | None:
+    """
+    Fetch a single recipe by ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result[0]
 
 
 app.include_router(api_router)
